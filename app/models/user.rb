66548718fd4c5ba64c5547e4512e72b1519992fa
斉藤文哉 <saitofumiya@saitoubunnarinoMacBook-Air.local>
2021-02-19 14:50:46 +0900

    エラーメッセージの実装とテストコードの実装
    コミットが大きくなってしまいました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, presence: true,  format: { with:  /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :nickname, presence: true
+
+  validates :last_name_kanji,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_kanji,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :last_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
+
+  validates :birthday, presence: true
+
+
+  
+  
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    
+    transient do
+      person { Gimei.name }
+    end
+    first_name_kanji { person.first }
+    last_name_kanji { person.last }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
+    birthday { Faker::Date.backward }
+  end
+end
