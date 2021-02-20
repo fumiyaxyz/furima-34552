8814f429349236d4cec304139d59905d37987f67
斉藤文哉 <saitofumiya@saitoubunnarinoMacBook-Air.local>
2021-02-20 14:48:50 +0900

    itemsモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
+  
+  with_options presence: true do
+    validates  :name
+    validates  :description
+    validates  :price,  numericality: { only_integer: true,
+      greater_than: 299, less_than: 10000000
+    }
+    with_options numericality: { other_than: 1}
+      validates  :category_id
+      validates  :status_id
+      validates  :delivery_fee_id
+      validates  :prefecture_id
+      validates  :ship_days_id
+     end
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
