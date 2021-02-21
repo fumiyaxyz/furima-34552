73991603e5fe60b18ecc8eafa9cd8791d180bcd6
斉藤文哉 <saitofumiya@saitoubunnarinoMacBook-Air.local>
2021-02-21 13:36:20 +0900

    修正　１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,22 +33,51 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーのidが１を選択されていると投稿できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
       it '商品の状態が空だと投稿できない' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '商品の状態のidが１を選択されていると投稿できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担が空だと投稿できない' do
         @item.delivery_fee_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
+      it '配送料の負担のidが１を選択されていると投稿できない' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
       it '発送元の地域が空だと投稿できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-
+      it '発送元の地域のidが１を選択されていると投稿できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '発送までの日数が空だと投稿できない' do
+        @item.ship_days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship days can't be blank")
+      end
+      it '発送までの日数のidが１を選択されていると投稿できない' do
+        @item.ship_days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship days must be other than 1")
+      end
       it '販売価格が空だと投稿できない' do
         @item.price = ''
         @item.valid?
